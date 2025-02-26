d2bf0693caef38e9eeeeab7c23bf32da0e89fb56
Javier Alberto Vásquez Suárez <134559483+JaviVaSu@users.noreply.github.com>
2025-02-26 13:59:36 -0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    aux = 0;
+
+    $("#openmenu").click(function(){
+        switch(aux){
+            case 0:
+                $("#menu").css("display", "flex");
+                $("#openmenu").attr("src", "./assets/images/icon-close.svg");
+                $("#body").css("overflow-y", "hidden")
+                aux = 1;
+                break
+            case 1:
+                $("#menu").css("display", "none");
+                $("#openmenu").attr("src", "./assets/images/icon-hamburger.svg");
+                $("#body").css("overflow-y", "scroll")
+                aux = 0;
+                break
+        }
+    })
+});
